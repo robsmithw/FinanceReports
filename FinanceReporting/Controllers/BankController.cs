df228ffaa7d57f43b7ce96dd4d84f8ff5b65ea49
robert <>
2019-07-20 21:01:49 -0500

    Init for viewing all banks and adding banks

---
+++ FinanceReporting/Controllers/BankController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceReporting.Controllers
+{
+    public class BankController : Controller
+    {
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
